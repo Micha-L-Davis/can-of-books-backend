8dcda4f99f00817ee2206e45c603d0e7c9477f89
Micha Davis <m.leigh.davis@gmail.com>
2022-02-28 17:12:30 -0800

    create books route

--- server.js
+++ server.js
@@ -3,16 +3,37 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL);
+const Book = require('./models/book');
 
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.get('/test', (request, response) => {
 
-  response.send('test request received')
+  response.send('test request received');
 
-})
+});
+
+app.get('/books', async (request, response) => {
+  try {
+    console.log(request);
+    const bookQuery = {};
+    if (request.query.email) {
+      bookQuery.email = request.query.email;
+    }
+
+    const books = await Book.find(bookQuery);
+
+    response.status(200).send(books);
+  }
+  catch (error) {
+    console.log(error.message);
+    response.status(500).send(error.message);
+  }
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
